fa36671e0ee9908a04f138c8547984649eb344db
Trenton Cary <johntcary@gmail.com>
2015-07-08 14:37:26 -0500

    add get_search_results

--- bookshout_api_wrapper/lib/bookshout_api_wrapper.rb
+++ bookshout_api_wrapper/lib/bookshout_api_wrapper.rb
@@ -2,8 +2,14 @@ require "bookshout_api_wrapper/version"
 require 'rest-client'
 
 module BookshoutApiWrapper
-  def self.get_info(user_id)
+  def self.get_user_info(user_id)
 	response = RestClient.get "www.bookshout.com/api/users/#{user_id}.json"
 	JSON.parse response
+  end
+  
+  def self.get_search_results(search_term)
+	results = RestClient.get "www.bookshout.com/api/books/search.json", :params => {:query => search_term}
+	JSON.parse results
+	
   end
 end
